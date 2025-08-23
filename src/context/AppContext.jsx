0908f7f38f82ee15ee9d079ef47205fabb1d03ff
dnyanu25 <dnyaneshwari1fce@gmail.com>
2025-08-23 23:27:16 +0530

    fc

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext,useContext,useEffect,useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
 
@@ -10,65 +10,89 @@ export const AppContext = createContext();
 export const AppContextProvider = ({ children }) => {
     const currency = import.meta.VITE_CURRENCY;
 
-    const navigate= useNavigate();
-    const [user,setUser]=useState(null)
-    const [isSeller,setIsSeller]=useState(false)
-    const [showUserLogin,setShowUserLogin]=useState(false)
-    const [Products,setProducts]=useState([])
-    const[cartItems,setCartItems]=useState({})
-    const[searchQuery,setSearchQuery]=useState({})
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null)
+    const [isSeller, setIsSeller] = useState(false)
+    const [showUserLogin, setShowUserLogin] = useState(false)
+    const [Products, setProducts] = useState([])
+    const [cartItems, setCartItems] = useState({})
+    const [searchQuery, setSearchQuery] = useState({})
     //fetch all products
-            const fetchProducts= async()=>{
-            setProducts(dummyProducts)
-        };
+    const fetchProducts = async () => {
+        setProducts(dummyProducts)
+    };
 
-//Add product to cart
-const addToCart= (itemId)=>{
-    let cartData = structuredClone(cartItems);
-    if( cartData[itemId]){
-        cartData[itemId]+=1;
-    }else{
-        cartData[itemId]=1;
-    }
-    setCartItems(cartData);
-    toast.success("Added to cart successfully");
-}
-        
-//update cart item quantity 
-const updateCartItem = (itemId, quantity) => {
-    let cartData = structuredClone(cartItems);
-    cartData[itemId] = quantity;
-    setCartItems(cartData);
-    toast.success("Cart updated successfully");
-}
-// remove product from cart
-const removeFromCart = (itemId) => {
-
-     let cartData = structuredClone(cartItems);
-    if(cartData[itemId]){
-        cartData[itemId] -= 1;
-        if(cartData[itemId]===0){
-            delete cartData[itemId];
+    //Add product to cart
+    const addToCart = (itemId) => {
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            cartData[itemId] += 1;
+        } else {
+            cartData[itemId] = 1;
         }
+        setCartItems(cartData);
+        toast.success("Added to cart successfully");
     }
-    toast.success("Item removed from cart");
-    setCartItems(cartData);
-}
 
-        useEffect( ()=>{
-             fetchProducts();
-        },[])
-      
-
-    const value = {navigate,user,setUser,setIsSeller,isSeller
-        ,showUserLogin,setShowUserLogin ,Products,currency, addToCart 
-        ,updateCartItem ,removeFromCart ,cartItems ,searchQuery,setSearchQuery
+    //update cart item quantity 
+    const updateCartItem = (itemId, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId] = quantity;
+        setCartItems(cartData);
+        toast.success("Cart updated successfully");
     }
-    
+    // remove product from cart
+    const removeFromCart = (itemId) => {
+
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            cartData[itemId] -= 1;
+            if (cartData[itemId] === 0) {
+                delete cartData[itemId];
+            }
+        }
+        toast.success("Item removed from cart");
+        setCartItems(cartData);
+    }
+
+
+    //Get cartitem count
+    const getCartCount = () => {
+        let totalcount = 0;
+        for (const item in cartItems) {
+            totalcount += cartItems[item];
+        }
+        return totalcount;
+    }
+    //total cart amount
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = Products.find((product1) => product1._id === items);
+            if (cartItems[items] > 0) {
+                totalAmount += itemInfo.offerPrice * cartItems[items]
+            }
+        }
+        return Math.floor(totalAmount * 100) / 100;
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+
+    const value = {
+        navigate, user, setUser, setIsSeller, isSeller
+        , showUserLogin, setShowUserLogin, Products, currency, addToCart
+        , updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery,getCartAmount
+        ,getCartCount
+    }
+
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
 }
 export const useAppContext = () => {
-  return useContext(AppContext); 
+    return useContext(AppContext);
 }
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import { use, useEffect, useState } from "react";
 import { useAppContext } from "../context/AppContext";
 import { Link, useParams } from "react-router-dom";
 import { assets } from "../assets/assets";
+import ProductCard from "../components/ProductCard";
 
 const ProductDetails = () => {
 
@@ -78,12 +79,11 @@ const ProductDetails = () => {
                             <li key={index}>{desc}</li>
                         ))}
                     </ul>
-
                     <div className="flex items-center mt-10 gap-4 text-base">
                         <button onClick={() => addToCart(product1._id)} className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition" >
                             Add to Cart
                         </button>
-                        <button onClick={() => { addToCart(product1._id); navigate("/cart") }} className="w-full py-3.5 cursor-pointer font-medium bg-indigo-500 text-white hover:bg-indigo-600 transition" >
+                        <button onClick={() => { addToCart(product1._id); navigate("/cart") }} className="w-full py-3.5 cursor-pointer font-medium bg-primary text-white hover:bg-primary-dull transition" >
                             Buy now
                         </button>
                     </div>
@@ -91,11 +91,23 @@ const ProductDetails = () => {
             </div>
             {/* related products */}
             <div className="flex flex-col items-center mt-20">
-                <div className="flex flex-col items-center w-max" >
+                <div className="flex flex-col items-center w-max">
                     <p className="text-3xl font-medium">Related Products</p>
                     <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
                 </div>
+
+                {/* Wrap product cards in a flex row */}
+                <div className="flex flex-wrap justify-center gap-6 mt-8">
+                    {relatedProducts
+                        .filter((product1) => product1.inStock)
+                        .map((product1, index) => (
+                            <ProductCard key={index} product={product1} />
+                        ))}
+                </div>
             </div>
+
+            <button onClick={() => { navigate('/products'); scrollTo(0, 0) }} className="mx-auto cursor-pointer px-12 my-16 py-2.5 border 
+            rounded text-primary hover:bg-primary/10 transition">See More</button>
 
         </div>
     );
